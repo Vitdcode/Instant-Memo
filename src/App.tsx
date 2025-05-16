c11604b3baa32e848e6a59cc9cfe1c0392436e38
Vitdcode <vitalijdietrich@gmail.com>
2025-05-16 19:29:34 +0200

    feat: included some missing dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import { StatusBar } from "expo-status-bar";
-import { Appearance, StyleSheet, Text, useColorScheme, View } from "react-native";
 import merge from "deepmerge";
-import { Button, MD3DarkTheme, MD3LightTheme, PaperProvider } from "react-native-paper";
-import * as SystemUI from "expo-system-ui";
 import * as NavigationBar from "expo-navigation-bar";
+import { StatusBar } from "expo-status-bar";
+import * as SystemUI from "expo-system-ui";
+import { useEffect } from "react";
+import { Appearance, StyleSheet, Text, useColorScheme, View } from "react-native";
+import { Button, MD3DarkTheme, MD3LightTheme, PaperProvider } from "react-native-paper";
 import Fab from "./components/Fab";
 import Colors from "./constants/Colors";
-import { useEffect } from "react";
-import { SafeAreaProvider } from "react-native-safe-area-context";
 
 const CombinedLightTheme = merge(MD3LightTheme, { colors: Colors.light });
 const CombinedDarkTheme = merge(MD3DarkTheme, { colors: Colors.dark });
@@ -48,11 +47,13 @@ export default function App() {
   });
 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your appp!</Text>
-      <Button mode="contained">This is new</Button>
-      <Fab />
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider theme={theme}>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your appp!</Text>
+        <Button mode="contained">This is new</Button>
+        <Fab />
+        <StatusBar style="auto" />
+      </View>
+    </PaperProvider>
   );
 }
