12a21aca783830994caf3db949f8e9d8a649e8d3
Vitdcode <vitalijdietrich@gmail.com>
2025-05-24 15:31:10 +0200

    feat: redesigned the app

--- src/components/MemoInputMenu.tsx
+++ src/components/MemoInputMenu.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useRef, useState } from "react";
-import { KeyboardAvoidingView, Pressable, View } from "react-native";
+import { Image, KeyboardAvoidingView, Pressable, View } from "react-native";
 import { Button, Text, TextInput, useTheme } from "react-native-paper";
 import * as Notifications from "expo-notifications";
 import { useAppContext } from "../Context";
+import { MaterialIcons } from "@expo/vector-icons";
 
 const MemoMenu: React.FC = () => {
   const theme = useTheme();
@@ -50,47 +51,101 @@ const MemoMenu: React.FC = () => {
   }, []);
 
   return (
-    <KeyboardAvoidingView style={{ flex: 1, width: "100%", marginBottom: 30 }} behavior="height">
+    <>
       <View
         style={{
-          width: "90%",
-          height: "90%",
-          justifyContent: "flex-end",
-          marginHorizontal: "auto",
-          marginBottom: 300,
-          gap: 40,
+          height: 120,
+          backgroundColor: theme.colors.background,
+          elevation: 5,
+          width: "100%",
+          flexDirection: "row",
+          gap: 20,
+          alignItems: "flex-end",
         }}
       >
-        <MemoCreated memoCreatedTextVisible={memoCreatedTextVisible} />
-        <TextInput
-          ref={textInputRef}
-          mode="flat"
-          label="Neue Memo"
-          value={notificationInput}
-          onChangeText={setNotificationInput}
-          onSubmitEditing={handleSubmit}
-          returnKeyType="send"
-          submitBehavior="submit"
-          cursorColor={theme.colors.secondary}
-          underlineColor={theme.colors.secondary}
-          activeUnderlineColor={theme.colors.secondary}
-          style={{
-            backgroundColor: theme.colors.inverseOnSurface,
-            width: "90%",
-            height: 70,
-            marginHorizontal: "auto",
-          }}
+        <Image
+          source={require("../../assets/in-app-icon.png")}
+          style={{ width: 40, height: 40, marginLeft: 20, marginBottom: 14 }}
         />
-        <Button
-          mode="contained"
-          onPress={handleSubmit}
-          buttonColor={theme.colors.onBackground}
-          style={{ width: "80%", height: 45, justifyContent: "center", marginHorizontal: "auto" }}
+        <Text
+          variant="titleLarge"
+          style={{
+            fontWeight: "bold",
+            color: theme.colors.onSurfaceVariant,
+            marginBottom: 20,
+          }}
         >
-          Memo erstellen
-        </Button>
+          Instant Memo
+        </Text>
+        <MemoCreated memoCreatedTextVisible={memoCreatedTextVisible} />
       </View>
-    </KeyboardAvoidingView>
+      <KeyboardAvoidingView
+        style={{
+          flex: 1,
+          width: "100%",
+        }}
+        behavior="padding"
+      >
+        <View
+          style={{
+            width: "90%",
+            backgroundColor: theme.colors.onPrimary,
+            borderRadius: 20,
+            padding: 10,
+            paddingTop: 20,
+            elevation: 20,
+            justifyContent: "flex-end",
+            marginHorizontal: "auto",
+            marginTop: "auto",
+            marginBottom: 50,
+            gap: 40,
+          }}
+        >
+          <Text
+            variant="titleLarge"
+            style={{ color: "rgb(36, 172, 235)", fontWeight: "bold", marginLeft: 20 }}
+          >
+            Neue Memo erstellen
+          </Text>
+          <TextInput
+            ref={textInputRef}
+            mode="outlined"
+            label="Neue Memo"
+            value={notificationInput}
+            onChangeText={setNotificationInput}
+            onSubmitEditing={handleSubmit}
+            returnKeyType="send"
+            submitBehavior="submit"
+            cursorColor={theme.colors.secondary}
+            underlineColor={theme.colors.secondary}
+            activeUnderlineColor={theme.colors.secondary}
+            outlineColor="rgba(0, 0, 0, 0.07)"
+            activeOutlineColor="rgba(97, 97, 97, 0.88)"
+            style={{
+              backgroundColor: theme.colors.inverseOnSurface,
+              width: "90%",
+              height: 120,
+              marginHorizontal: "auto",
+            }}
+          />
+          <Button
+            mode="contained"
+            onPress={handleSubmit}
+            buttonColor="rgb(14, 165, 233)"
+            textColor="white"
+            style={{
+              width: "80%",
+              height: 45,
+              justifyContent: "center",
+              marginHorizontal: "auto",
+              marginBottom: 15,
+            }}
+          >
+            Memo erstellen
+          </Button>
+        </View>
+      </KeyboardAvoidingView>
+    </>
   );
 };
 
@@ -102,19 +157,26 @@ const MemoCreated: React.FC<MemoCreatedProps> = ({ memoCreatedTextVisible }) => 
   const theme = useTheme();
   return (
     memoCreatedTextVisible && (
-      <Text
+      <View
         style={{
           marginHorizontal: "auto",
-          backgroundColor: theme.colors.secondary,
-          /*     elevation: 5, */
-          color: "white",
+          backgroundColor: theme.colors.onPrimary,
+          elevation: 1,
           padding: 10,
           borderRadius: 10,
-          fontSize: 15,
+          /* borderWidth: 1,
+          borderColor: "rgba(0, 0, 0, 0.06)", */
+          flexDirection: "row",
+          gap: 10,
+          alignItems: "center",
+          marginBottom: 10,
+          position: "absolute",
+          right: 10,
         }}
       >
-        Memo erstellt
-      </Text>
+        <Text style={{ color: theme.colors.onSurfaceVariant, fontSize: 15 }}>Memo erstellt</Text>
+        <MaterialIcons name="check-circle" size={25} color="rgb(0, 164, 232)" />
+      </View>
     )
   );
 };
